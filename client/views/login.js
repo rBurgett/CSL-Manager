2ef22dbf612c9dd01930423ad95105612123ccec
rBurgett <ryan@burgettweb.net>
2014-10-26 20:16:54 -0400

    Added submit event handler to login template

---
+++ client/views/login.js
@@ -0,0 +1,19 @@
+Template.login.helpers({
+
+});
+Template.login.events({
+    'submit form': function(event) {
+        event.preventDefault();
+        var userData = {
+            username: $(event.target).find('[name=username]').val(),
+            password: $(event.target).find('[name=password]').val()
+        }
+	Meteor.call('login', userData, function(error) {
+            if (error) {
+                error.throw(error);
+            } else {
+                router.go(dashboard);
+            }
+        }
+    }
+});
