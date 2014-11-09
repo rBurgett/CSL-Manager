0eea080f2419521524acc0bf56c1de309432107b
Ryan <ryan@burgettweb.net>
2014-11-09 14:34:58 -0500

    Added times page

--- client/views/dashboard/shared/dashSideBar.js
+++ client/views/dashboard/shared/dashSideBar.js
@@ -1,6 +1,8 @@
 Template.dashSideBar.helpers({
 	checkInOutPage : function() {
 		if (Session.get('activePage') === 'checkInOutPage') return true;
-		
+	},
+	times : function() {
+		if (Session.get('times') === 'checkInOutPage') return true;
 	}
 });
