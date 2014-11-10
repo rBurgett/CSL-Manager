9105966b3ef897c2628eddc7089428120df5d76a
Ryan <ryan@burgettweb.net>
2014-11-09 19:58:37 -0500

    Changed dashboard.html

--- client/views/dashboard/dashboard.js
+++ client/views/dashboard/dashboard.js
@@ -1,6 +1,13 @@
 Template.dashboard.helpers({
 	name: function() {
 		return ', ' + Meteor.user().profile.firstName;
+	},
+	checkedIn: function() {
+		if (Meteor.user().profile.checkedIn) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 });
 Template.dashboard.events({
