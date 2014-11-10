dae22cc4b02f2a1539a587298da3f913a5cf5d49
Ryan <ryan@burgettweb.net>
2014-11-09 22:32:28 -0500

    Configured check in/out logs

--- collections/methods.js
+++ collections/methods.js
@@ -1,10 +1,21 @@
 Meteor.methods({
 	checkInOut: function (userId) {
+		var status, log;
 		var checkedIn = Meteor.user().profile.checkedIn;
+		var time = new Date().getTime();
 		if (checkedIn) {
 			Meteor.users.update({_id: userId}, {$set: {'profile.checkedIn': false}});
+			status = 'out';
+			
 		} else {
 			Meteor.users.update({_id: userId}, {$set: {'profile.checkedIn': true}});
+			status = 'in';
 		}
+		log = {
+			status: status,
+			userId: userId,
+			time: time;
+		}
+		TimeLogs.insert(log);
 	}
 });
