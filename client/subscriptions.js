414c73e6187f8d50dd6320d169ae0a30db7dc2de
Ryan <ryan@burgettweb.net>
2014-11-09 22:48:11 -0500

    Added timeLogs publication and subscription

--- client/subscriptions.js
+++ client/subscriptions.js
@@ -1,1 +1,2 @@
 Meteor.subscribe('checkedInUsers');
+Meteor.subscribe('timeLogs');
--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,6 @@
 Meteor.publish('checkedInUsers', function() {
 	return Meteor.users.find();
 });
+Meteor.publish('timeLogs', function() {
+	return TimeLogs.find();
+});
