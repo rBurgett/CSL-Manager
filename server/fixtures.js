c15b764bbd740ccb841e418b3ddb6b3be1757069
Ryan <ryan@burgettweb.net>
2014-11-10 22:39:34 -0500

    Changed publications and subscriptions

--- server/fixtures.js
+++ server/fixtures.js
@@ -9,4 +9,24 @@ if (Meteor.users.find().count() === 0) {
             'lastName' : null
         }
     });
+	Accounts.createUser({
+        'username' : 'manager',
+        'password' : 'managerpass',
+        'profile' : {
+            'role' : 'manager',
+            'checkedIn' : false,
+            'firstName' : null,
+            'lastName' : null
+        }
+    });
+	Accounts.createUser({
+        'username' : 'user',
+        'password' : 'userpass',
+        'profile' : {
+            'role' : 'user',
+            'checkedIn' : false,
+            'firstName' : null,
+            'lastName' : null
+        }
+    });
 };
