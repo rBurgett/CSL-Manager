f0d920cea42da086da8fabd49443adf80b7df06f
Ryan <ryan@burgettweb.net>
2014-11-12 21:42:15 -0500

    Added pagination to times.html

--- client/views/dashboard/times/times.js
+++ client/views/dashboard/times/times.js
@@ -1,11 +1,18 @@
 Template.times.helpers({
 	log: function() {
 		return TimeLogs.find({userId: Meteor.userId()}, {sort: {time: -1}, limit: Session.get('logsDisplayed')});
+	},
+	more: function() {
+		if (TimeLogs.find({userId: Meteor.userId()}).count() > 20) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 });
 Template.times.events({
-	'click #viewMoreLogs': function() {
-		var l = Session.get('logsDisplayed') + 10;
+	'click #loadMoreLogs': function() {
+		var l = Session.get('logsDisplayed') + 20;
 		Session.set('logsDisplayed', l);
 	}
 });
